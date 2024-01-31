d02a6d57ab4b2b3d92afc5f268cd0f6a0c5733e6
CommitTODev <abhishekshivale45@gmail.com>
2024-01-31 11:44:27 +0530

    home done

--- ecommerce-frontend/src/components/Header.tsx
+++ ecommerce-frontend/src/components/Header.tsx
@@ -1,11 +1,42 @@
 import {Link} from 'react-router-dom'
-import { FaSearch, FaShoppingBag } from 'react-icons/fa'
+import { FaSearch, FaShoppingBag, FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
+import { useState } from 'react'
 
+const user = {_id:"rffgrf", role :"admin"}
 function Header() {
-  return (<nav>
-    <Link to={'/'} >Home</Link>
-    <Link to={'/search'} ><FaSearch/>{''}</Link>
-    <Link to={'/cart'} ><FaShoppingBag/>{''}</Link>
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+
+  const logoutHandler = () => {
+    setIsOpen(false);
+  }
+
+  return (<nav className='header'>
+    <Link to={'/'} onClick={()=>{setIsOpen(false)}}>Home</Link>
+    <Link to={'/search'} onClick={()=>{setIsOpen(false)}}><FaSearch/></Link>
+    <Link to={'/cart'} onClick={()=>{setIsOpen(false)}}><FaShoppingBag/></Link>
+    {
+      user?._id?(
+        <>
+        <button onClick={()=>{setIsOpen((pre)=> !pre)}}><FaUser/></button>
+        <dialog open={isOpen}>
+          <div>
+            {
+              user.role === 'admin' && (
+                <Link onClick={()=>{setIsOpen(false)}} to={'/admin/dashboard'}>Admin</Link>
+              )
+            }
+            <Link onClick={()=>{setIsOpen(false)}} to={'/orders'}>My Orders</Link>
+            <button onClick={logoutHandler}><FaSignOutAlt /></button>
+          </div>
+        </dialog>
+        </>
+      ):(
+        <>
+        <Link onClick={()=>{setIsOpen(false)}} to={'/login'} ><FaSignInAlt/></Link>
+       </>
+      )
+    }
+
   </nav>)
 }
 
--- ecommerce-frontend/src/pages/Home.tsx
+++ ecommerce-frontend/src/pages/Home.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import ProductCard from '../components/product-card.tsx'
 
 function Home() {
+  const handle = () =>{
+
+  }
   return (
-    <div>Home</div>
+    <div className='home'>
+      <section>
+
+      </section>
+      <h1>
+        Latest Product
+        <Link to={'/search'} className='findmore'>More</Link>
+      </h1>
+      <main>
+      <ProductCard productId={'asdsa'} photo={'https://m.media-amazon.com/images/I/71eXNIDUGjL._AC_UY218_.jpg'} name='macbook' price={127} stock={1232} handler={handle} />
+      </main>
+    </div>
   )
 }
 
